6ff28e86b8b1d3780543f16aa3ae48b73d8045b2
DaviLimaPereira <39305143+DaviLimaPereira@users.noreply.github.com>
2019-05-28 23:30:22 -0300

    Classes
    Conexão de banco e Users

--- src/java/br/com/fatecpg/school/DbConnector.java
+++ src/java/br/com/fatecpg/school/DbConnector.java
@@ -15,4 +15,34 @@ public class DbConnector {
     private static final String URL = "jdbc:derby://localhost:1527/school";
     private static final String PASS = "school";
     
+    public static void execute(String SQL, Object[] parameters) throws Exception{
+        Class.forName(DRIVER);
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        PreparedStatement stmt = con.prepareStatement(SQL);
+        for(int i=0; i < parameters.length; i++){
+            stmt.setObject(i+1, parameters[i]);
+        }
+        stmt.execute();
+        stmt.close();con.close();
+    }
+    
+    public static ArrayList<Object[]> getQuery(String SQL, Object[] parameters) throws Exception{
+        ArrayList<Object[]> list = new ArrayList<>();
+        Class.forName(DRIVER);
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        PreparedStatement stmt = con.prepareStatement(SQL);
+        for(int i=0; i < parameters.length; i++){
+            stmt.setObject(i+1, parameters[i]);
+        }
+        ResultSet rs = stmt.executeQuery();
+        while(rs.next()){
+            Object row[] = new Object[rs.getMetaData().getColumnCount()];
+            for(int i=0; i < rs.getMetaData().getColumnCount(); i++){
+                row[i] = rs.getObject(i+1);
+            }
+            list.add(row);
+        }
+        rs.close();stmt.close();con.close();
+        return list;
+    }
 }
